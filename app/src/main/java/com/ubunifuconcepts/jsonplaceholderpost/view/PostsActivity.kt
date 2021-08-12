e33e9a98cd93e8639df387bb0dbb55f57a9c5ae9
Brian <b.onyango@kokonetworks.com>
2021-08-12 19:17:40 +0300

    Created a skeleton PostActivity and its layout

---
+++ app/src/main/java/com/ubunifuconcepts/jsonplaceholderpost/view/PostsActivity.kt
@@ -0,0 +1,12 @@
+package com.ubunifuconcepts.jsonplaceholderpost.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ubunifuconcepts.jsonplaceholderpost.R
+
+class PostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts)
+    }
+}
