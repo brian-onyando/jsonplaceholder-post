deba555b54718f4620c552e778c4de55a112121f
Brian <b.onyango@kokonetworks.com>
2021-08-12 18:25:23 +0300

    Added a Post model data class + gson dependency

---
+++ app/src/main/java/com/ubunifuconcepts/jsonplaceholderpost/model/Post.kt
@@ -0,0 +1,18 @@
+package com.ubunifuconcepts.jsonplaceholderpost.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Post(
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("title")
+    val title: String,
+
+    @field:SerializedName("body")
+    val body: String,
+
+    @field:SerializedName("userId")
+    val userId: Int
+)
